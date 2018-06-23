882e38837902cd224d582a4ec249a0d923d8f061
Chatchai Saratakij <CSaratakij@gmail.com>
2018-06-23 10:40:46 +0700

    Process input from gamepad axis and eliminate in-between range from (1.0, -1.0).

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SC
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        float moveSpeed;
+
+        [SerializeField]
+        float gamepadDeadZone;
+
+
+        float axisX;
+        float axisY;
+
+        Vector2 inputAxis;
+        Rigidbody2D rigid;
+
+
+        void Awake()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            _Input_Handler();
+        }
+
+        void FixedUpdate()
+        {
+        }
+
+        void _Input_Handler()
+        {
+            axisX = Input.GetAxisRaw("Horizontal");
+
+            if (axisX > gamepadDeadZone) {
+                inputAxis.x = 1.0f;
+            }
+            else if (axisX < -gamepadDeadZone) {
+                inputAxis.x = -1.0f;
+            }
+            else {
+                inputAxis.x = 0.0f;
+            }
+
+            axisY = Input.GetAxisRaw("Vertical");
+
+            if (axisY > gamepadDeadZone) {
+                inputAxis.y = 1.0f;
+            }
+            else if (axisY < -gamepadDeadZone) {
+                inputAxis.y = -1.0f;
+            }
+            else {
+                inputAxis.y = 0.0f;
+            }
+        }
+    }
+}
