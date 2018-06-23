cafe7c431b78042eaa27b4125c2a8d0bf8ca9177
Chatchai Saratakij <CSaratakij@gmail.com>
2018-06-23 11:24:56 +0700

    Added GameClock.

---
+++ Assets/Scripts/Game/GameClock.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SC
+{
+    public class GameClock : MonoBehaviour
+    {
+        Timer timer;
+
+
+        void Awake()
+        {
+            timer = GetComponent<Timer>();
+            _Subscribe_Events();
+        }
+
+        void OnDestroy()
+        {
+            _Unsubscribe_Events();
+        }
+
+        void _OnGameStart()
+        {
+            timer.Countdown();
+        }
+
+        void _OnTimerStopped()
+        {
+            GameController.GameOver();
+        }
+
+        void _Subscribe_Events()
+        {
+            if (timer) {
+                timer.OnTimerStopped += _OnTimerStopped;
+            }
+            else {
+                Debug.Log("Can't find timer..");
+            }
+
+            GameController.OnGameStart += _OnGameStart;
+        }
+
+        void _Unsubscribe_Events()
+        {
+            if (timer) {
+                timer.OnTimerStopped -= _OnTimerStopped;
+            }
+            else {
+                Debug.Log("Can't find timer..");
+            }
+
+            GameController.OnGameStart -= _OnGameStart;
+        }
+    }
+}
