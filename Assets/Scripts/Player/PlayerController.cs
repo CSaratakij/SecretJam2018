02ce833a2f3a01462cb7e7eab753c46310fb134d
Chatchai Saratakij <CSaratakij@gmail.com>
2018-06-24 03:58:57 +0700

    Tweaks player control.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,9 @@ namespace SC
 {
     public class PlayerController : MonoBehaviour
     {
+        const float X_OFFSET = 0.5f;
+        const float Y_OFFSET = -0.2f;
+
         [SerializeField]
         float moveSpeed;
 
@@ -20,22 +23,31 @@ namespace SC
         Rigidbody2D rigid;
 
 
+        Vector3 expectPos;
+
+
         void Awake()
         {
             rigid = GetComponent<Rigidbody2D>();
         }
 
+        void Start()
+        {
+            expectPos = rigid.position;
+        }
+
         void Update()
         {
+            _Input_Processing();
             _Input_Handler();
         }
 
         void FixedUpdate()
         {
-
+            _Movement_Handler();
         }
 
-        void _Input_Handler()
+        void _Input_Processing()
         {
             axisX = Input.GetAxisRaw("Horizontal");
 
@@ -60,6 +72,30 @@ namespace SC
             else {
                 inputAxis.y = 0.0f;
             }
+        }
+
+        void _Input_Handler()
+        {
+            var distance = Vector3.Distance(rigid.position, expectPos);
+            var deadZone = 0.1f;
+
+            if (distance >= deadZone) {
+                return;
+            }
+
+            if (inputAxis.magnitude > 0.0f) {
+                expectPos = expectPos + new Vector3(inputAxis.x, inputAxis.y, 0.0f);
+            }
+        }
+
+        void _Movement_Handler()
+        {
+            if (expectPos == transform.position) {
+                return;
+            }
+
+            var lerpVector = Vector3.Lerp(rigid.position, expectPos, 0.18f);
+            rigid.position = lerpVector;
         }
     }
 }
