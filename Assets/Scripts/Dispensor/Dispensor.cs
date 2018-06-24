6f567969ee86d80ef7a31489ac7f5d39a2a29a4f
Chatchai Saratakij <CSaratakij@gmail.com>
2018-06-25 03:08:09 +0700

    Added fancy mainmenu.

--- Assets/Scripts/Dispensor/Dispensor.cs
+++ Assets/Scripts/Dispensor/Dispensor.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
 namespace SC
 {
     public class Dispensor : MonoBehaviour
@@ -33,6 +37,15 @@ namespace SC
 
         Timer timer;
 
+#if UNITY_EDITOR
+
+         void OnDrawGizmos() {
+            if (switchComponent) {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(transform.position, switchComponent.gameObject.transform.position);
+            }
+         }
+#endif
 
         void Awake()
         {
---
+++ Assets/Scripts/UI/StockHealthView.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SC
+{
+    public class StockHealthView : MonoBehaviour
+    {
+        [SerializeField]
+        Image[] imgHearts;
+
+        [SerializeField]
+        StockHealth stockHealth;
+
+
+        void Awake()
+        {
+            if (stockHealth) {
+                stockHealth.OnStockHealthChanged += _OnStockHealthChanged;
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (stockHealth) {
+                stockHealth.OnStockHealthChanged -= _OnStockHealthChanged;
+            }
+        }
+
+        void _OnStockHealthChanged(int value)
+        {
+            Show(value);
+        }
+
+        public void Show(int value)
+        {
+            for (int i = 0; i < imgHearts.Length; i++) {
+                if (i + 1 <= value) {
+                    imgHearts[i].gameObject.SetActive(true);
+                }
+                else {
+                    imgHearts[i].gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
